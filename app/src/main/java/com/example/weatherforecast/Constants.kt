011269722d17d2bb14e5a70185eda777982d2ba3
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-04-02 04:43:33 +0200

    finish sharedPreference

--- app/src/main/java/com/example/weatherforecast/Constants.kt
+++ app/src/main/java/com/example/weatherforecast/Constants.kt
@@ -3,4 +3,18 @@ package com.example.weatherforecast
 object Constants {
     const val API_KEY = "07499aba09bc7f760e6f15af565f5b13"
     const val EXCLUDE="minutely"
+    const val SHARED_PREFERENCES = "SHARED_PREFERENCE"
+    var LOCATION = "LOCATION"
+    var LANGUAGE = "ENGLISH"
+    var TEMPERATURE = "TEMPERATURE"
+    var WIND_SPEED = "WIND_SPEED"
+    var UNIT = "UNIT"
+    var NOTIFICATION = "NOTIFICATION"
+    var MAP = "MAP"
+    var LAT_HOME = "LAT_HOME"
+    var LON_HOME = "LON_HOME"
+    var MAP_VALUE_LAT = "MAP_VALUE_LAT"
+    var MAP_VALUE_LON = "MAP_VALUE_LON"
+    var TIME = "TIME"
+    var ID = "ID"
 }
