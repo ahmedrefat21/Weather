f96f817bcf4e9ef2b21390d1ba8561e3169ef590
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-04-02 04:25:16 +0200

    finish Repository Implementation

--- app/src/main/java/com/example/weatherforecast/model/Repository.kt
+++ app/src/main/java/com/example/weatherforecast/model/Repository.kt
@@ -1,8 +1,27 @@
 package com.example.weatherforecast.model
 
-import WeatherResponse
+
+import com.example.weatherforecast.model.entity.AlertEntity
+import com.example.weatherforecast.model.entity.FavoriteEntity
+import com.example.weatherforecast.model.entity.HomeEntity
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
     suspend fun getWeather(lat: Double,lon: Double,units: String,lang:String) : Flow<WeatherResponse>
+
+    fun getCurrentWeather() : Flow<HomeEntity>
+    suspend fun insertCurrentWeather(homeEntity: HomeEntity)
+    suspend fun deleteCurrentWeather()
+
+
+    fun getFavoriteCity() : Flow<List<FavoriteEntity>>
+    suspend fun insertFavoriteCity(favoriteEntity : FavoriteEntity)
+    suspend fun deleteFavoriteCity(id : Int)
+
+    fun getAlert() : Flow<List<AlertEntity>>
+    suspend fun insertAlert(alertEntity: AlertEntity)
+    suspend fun deleteAlert(alertEntity: AlertEntity)
+    fun getAlertById(id:String): AlertEntity
+
+    suspend fun getWeatherAlert (lat: Double,lon: Double,units: String,lang:String) : WeatherResponse
 }
--- app/src/main/java/com/example/weatherforecast/model/RepositoryImpl.kt
+++ app/src/main/java/com/example/weatherforecast/model/RepositoryImpl.kt
@@ -1,23 +1,32 @@
 package com.example.weatherforecast.model
 
-import WeatherResponse
+
+import com.example.weatherforecast.local.LocalDataSource
+import com.example.weatherforecast.model.entity.AlertEntity
+import com.example.weatherforecast.model.entity.FavoriteEntity
+import com.example.weatherforecast.model.entity.HomeEntity
 import com.example.weatherforecast.network.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 class RepositoryImpl private constructor(
-    private var weatherRemoteDataSource: RemoteDataSource
-    ) : Repository {
+    private var remoteDataSource: RemoteDataSource,
+    private var localDataSource : LocalDataSource
+
+) : Repository {
 
 
     companion object{
         private var instance: RepositoryImpl? = null
         fun getInstance(
-            weatherRemoteDataSource: RemoteDataSource
+            weatherRemoteDataSource: RemoteDataSource,
+            localDataSource : LocalDataSource
+
         ) : RepositoryImpl {
             return instance ?: synchronized(this){
                 val temp = RepositoryImpl(
-                    weatherRemoteDataSource)
+                    weatherRemoteDataSource,
+                    localDataSource)
                 instance = temp
                 temp
             }
@@ -30,7 +39,47 @@ class RepositoryImpl private constructor(
         units: String,
         lang: String
     ): Flow<WeatherResponse> {
-        return flowOf(weatherRemoteDataSource.getWeatherOverNetwork(lat,lon,units,lang))
+        return flowOf(remoteDataSource.getWeatherOverNetwork(lat,lon,units,lang))
+    }
+
+    override fun getCurrentWeather(): Flow<HomeEntity> {
+        return localDataSource.getCurrentWeather()
+    }
+
+    override suspend fun insertCurrentWeather(homeEntity: HomeEntity) {
+        return localDataSource.insertCurrentWeather(homeEntity)
+    }
+
+    override suspend fun deleteCurrentWeather() {
+        return localDataSource.deleteCurrentWeather()
+    }
+
+
+    override fun getFavoriteCity(): Flow<List<FavoriteEntity>> {
+        return localDataSource.getFavouriteCity()
+    }
+
+    override suspend fun insertFavoriteCity(favoriteEntity: FavoriteEntity) {
+        return localDataSource.insertFavouriteCity(favoriteEntity)
+    }
+
+    override suspend fun deleteFavoriteCity(id : Int) {
+        return localDataSource.deleteFavouriteCity(id)
+    }
+
+    override fun getAlert(): Flow<List<AlertEntity>> = localDataSource.getAlert()
+    override suspend fun insertAlert(alertEntity: AlertEntity) {
+        localDataSource.insertAlert(alertEntity)
+    }
+    override suspend fun deleteAlert(alertEntity: AlertEntity) {
+        localDataSource.deleteAlert(alertEntity)
+    }
+    override fun getAlertById(id: String): AlertEntity {
+        return localDataSource.getAlertById(id)
+    }
+
+    override suspend fun getWeatherAlert(lat: Double,lon: Double,units: String,lang:String): WeatherResponse {
+        return remoteDataSource.getWeatherOverNetwork(lat,lon,units,lang)
     }
 
 
